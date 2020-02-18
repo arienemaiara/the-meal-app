e279c73e357b073734064995c645823b7b96391f
Ariene Maiara <ariene.maiara@hotmail.com>
2020-02-18 19:23:38 -0300

    Including category data

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -6,10 +6,22 @@ import {
     StyleSheet
 } from 'react-native';
 
+import { CATEGORIES } from '../data/dummy-data';
+
+const renderGridItem = (itemData) => {
+    return (
+        <View style={styles.gridItem}>
+            <Text>{itemData.item.title}</Text>
+        </View>
+    )
+};
+
 const CategoriesScreen = props => {
     return (
         <FlatList 
+            data={CATEGORIES}
             numColumns={2}
+            renderItem={renderGridItem}
         />
     );
 };
@@ -19,6 +31,11 @@ const styles = StyleSheet.create({
         flex: 1,
         alignItems: 'center',
         justifyContent: 'center',
+    },
+    gridItem: {
+        flex: 1,
+        margin: 15,
+        height: 150
     }
 });
 
