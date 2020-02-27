22d56e1044a58bb098af9a94be772d357aefe12c
Ariene Maiara <ariene.maiara@hotmail.com>
2020-02-26 22:41:20 -0300

    Adding bg image and stylind

--- components/MealItem.js
+++ components/MealItem.js
@@ -3,6 +3,7 @@ import {
     View,
     Text,
     TouchableOpacity,
+    ImageBackground,
     StyleSheet
 } from 'react-native';
 
@@ -13,7 +14,11 @@ const MealItem = props => {
                 onPress={props.onSelectMeal}>
                 <View>
                     <View style={{...styles.mealRow, ...styles.mealHeader}}>
-                        <Text>{props.title}</Text>
+                        <ImageBackground 
+                            source={{uri: props.image}}
+                            style={styles.bgImage}>
+                            <Text style={styles.title}>{props.title}</Text>
+                        </ImageBackground>
                     </View>
                     <View style={{...styles.mealRow, ...styles.mealDetail}}>
                         <Text>{props.duration}m</Text>
@@ -30,17 +35,34 @@ const styles = StyleSheet.create({
     mealItem: {
         height: 200,
         width: '100%',
-        backgroundColor: '#ccc'
+        backgroundColor: '#f5f5f5',
+        borderRadius: 10,
+        overflow: 'hidden',
     },
     mealRow: {
         flexDirection: 'row'
     },
     mealHeader: {
-        height: '90%'
+        height: '85%'
     },
     mealDetail: {
+        height: '15%',
         paddingHorizontal: 10,
-        justifyContent: 'space-between'
+        justifyContent: 'space-between',
+        alignItems: 'center'
+    },
+    bgImage: {
+        width: '100%',
+        height: '100%',
+        justifyContent: 'flex-end'
+    },
+    title: {
+        fontSize: 22,
+        color: 'white',
+        textAlign: 'center',
+        backgroundColor: 'rgba(0,0,0,0.5)',
+        paddingVertical: 5,
+        paddingHorizontal: 12
     }
 });
 
